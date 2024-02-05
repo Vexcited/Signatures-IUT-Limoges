f4072d57c0b52c16d33286e9f4d69256fdac7c65
Mikkel RINGAUD <mikkel@milescode.dev>
2024-02-05 22:59:54 +0100

    chore(web): prepare for deploy

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from "@solidjs/start/config";
 import unocss from "unocss/vite";
 
 export default defineConfig({
-  start: { ssr: false },
+  start: {
+    ssr: false,
+    server: { preset: "vercel" }
+  },
+
   // @ts-ignore : Average typing error between Vite and UnoCSS.
   plugins: [unocss()]
 });
