8f330c124a7fa4d2b91cc9db799ffabc19613044
Mikkel RINGAUD <mikkel@milescode.dev>
2024-02-06 07:45:36 +0100

    fix(web): add uno config

---
+++ packages/website/uno.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig, presetUno, transformerVariantGroup } from "unocss";
+import { presetKobalte } from "unocss-preset-primitives";
+
+export default defineConfig({
+  presets: [presetUno(), presetKobalte()],
+  transformers: [transformerVariantGroup()],
+
+  theme: {
+    colors: {
+      red: "rgba(var(--custom-color), %alpha)"
+    },
+
+    // breakpoints: {
+    //   tablet: "768px",
+    //   "laptop-sm": "1024px",
+    //   "laptop-lg": "1440px"
+    // },
+
+    animation: {
+      keyframes: {
+        "scale-in": "{from{opacity:0;transform:scale(0.96)}to{opacity:1;transform:scale(1)}}",
+        "scale-out": "{from{opacity:1;transform:scale(&)}to{opacity:0;transform:scale(0.96)}}"
+      }
+    }
+  }
+});
