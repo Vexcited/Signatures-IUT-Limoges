e18bba17423b54ca8129dbfa12405e2e12f60af9
Mikkel RINGAUD <mikkel@milescode.dev>
2024-03-01 17:06:36 +0100

    fix(web): refresh reload placement

--- packages/website/src/app.tsx
+++ packages/website/src/app.tsx
@@ -3,7 +3,6 @@ import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/500.css';
 import "virtual:uno.css";
 
-// @refresh reload
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start";
 import { Suspense } from "solid-js";
