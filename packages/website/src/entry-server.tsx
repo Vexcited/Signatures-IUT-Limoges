cfc4952e2bcdc0a8ef737b4a5ebd391d00266f7a
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-17 11:54:42 +0200

    feat(web): add meta

--- packages/website/src/entry-server.tsx
+++ packages/website/src/entry-server.tsx
@@ -7,9 +7,31 @@ export default createHandler(() => (
       <html lang="fr">
         <head>
           <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+          <meta name="color-scheme" content="dark light" />
+          <meta name="msapplication-TileColor" content="#f2f4f4" />
+          <meta name="theme-color" content="#f2f4f4" />
+
+          <title>Signatures - IUT de Limoges</title>
+          <meta name="title" content="Signatures - IUT de Limoges" />
+          <meta name="description"
+            content="Une PWA simple et fonctionnelle hors-ligne à utiliser pour visualiser ses notes n'importe quand de façon instantané." />
+          <link rel="canonical" href="https://signatures-iut-limoges.vercel.app" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://signatures-iut-limoges.vercel.app" />
+          <meta property="og:title" content="Signatures - IUT de Limoges" />
+          <meta property="og:description"
+            content="Une PWA simple et fonctionnelle hors-ligne à utiliser pour visualiser ses notes n'importe quand de façon instantané." />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://signatures-iut-limoges.vercel.app" />
+          <meta property="twitter:title" content="Signatures - IUT de Limoges" />
+          <meta property="twitter:description"
+            content="Une PWA simple et fonctionnelle hors-ligne à utiliser pour visualiser ses notes n'importe quand de façon instantané." />
+
           <link rel="icon" href="/favicon.ico" />
-          <title>Signatures IUT - Limoges</title>
           {assets}
         </head>
         <body class="bg-[rgb(18,18,18)] text-[rgb(250,250,250)]" style={{ "font-family": "'Poppins', sans-serif" }}>
