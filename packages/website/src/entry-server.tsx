9fc28233651c30371af09357d3ca124171b8cc7e
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-17 13:34:56 +0200

    chore(web): re-arrange tags

--- packages/website/src/entry-server.tsx
+++ packages/website/src/entry-server.tsx
@@ -10,27 +10,9 @@ export default createHandler(() => (
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
           <meta name="color-scheme" content="dark light" />
-          <meta name="msapplication-TileColor" content="#FFFFFF" />
           <meta name="theme-color" content="#FFFFFF" />
 
           <title>Signatures - IUT de Limoges</title>
-          <meta name="title" content="Signatures - IUT de Limoges" />
-          <meta name="description"
-            content="Une PWA simple et fonctionnelle hors-ligne à utiliser pour visualiser ses notes n'importe quand de façon instantané." />
-          <link rel="canonical" href="https://signatures-iut-limoges.vercel.app" />
-
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://signatures-iut-limoges.vercel.app" />
-          <meta property="og:title" content="Signatures - IUT de Limoges" />
-          <meta property="og:description"
-            content="Une PWA simple et fonctionnelle hors-ligne à utiliser pour visualiser ses notes n'importe quand de façon instantané." />
-
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://signatures-iut-limoges.vercel.app" />
-          <meta property="twitter:title" content="Signatures - IUT de Limoges" />
-          <meta property="twitter:description"
-            content="Une PWA simple et fonctionnelle hors-ligne à utiliser pour visualiser ses notes n'importe quand de façon instantané." />
-
           <link rel="icon" href="/favicon.ico" />
           {assets}
         </head>
