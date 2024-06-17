29ff3c39c5cf964586614f7d1c4b545df237c892
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-17 11:55:15 +0200

    feat(web): add colors utilities

--- packages/website/src/app.tsx
+++ packages/website/src/app.tsx
@@ -5,18 +5,36 @@ import "virtual:uno.css";
 
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start";
-import { Suspense } from "solid-js";
+import { Suspense, createEffect } from "solid-js";
 import { Toaster } from "solid-toast";
+import { preferences } from "./store/preferences";
+import { MetaProvider, Meta } from "@solidjs/meta";
+import { rgbToHex } from "./utils/colors";
 
 export default function App () {
+  const primaryColor = () => preferences.primaryColor;
+  const primaryColorHEX = () => primaryColor()
+    .split(",")
+    .map(i => parseInt(i.trim())) as [r: number, g: number, b: number];
+
+  createEffect(() => {
+    // setup the custom color from the user preferences.
+    const root = document.querySelector(':root') as HTMLElement;
+    root.style.setProperty('--custom-color', primaryColor());
+  });
+
   return (
     <>
       <Toaster />
       <Router
         root={props => (
-          <Suspense>
-            {props.children}
-          </Suspense>
+          <MetaProvider>
+            <Meta name="theme-color" content={rgbToHex(...primaryColorHEX())} />
+
+            <Suspense>
+              {props.children}
+            </Suspense>
+          </MetaProvider>
         )}
       >
         <FileRoutes />
