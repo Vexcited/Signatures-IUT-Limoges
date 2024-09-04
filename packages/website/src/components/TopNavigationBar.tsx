d59cbc9180bd077f08cef0913a1e0f89d4d72fdc
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-04 07:34:09 +0200

    fix: nav height when no semester is selected

--- packages/website/src/components/TopNavigationBar.tsx
+++ packages/website/src/components/TopNavigationBar.tsx
@@ -2,7 +2,7 @@ import type { FlowComponent } from "solid-js";
 
 const TopNavigationBar: FlowComponent = (props) => {
   return (
-    <nav class="z-40 sticky top-[77px] h-[36px] bg-[rgb(9,9,9)] border-b border-[rgb(40,40,40)]/40 container mx-auto flex h-full">
+    <nav class="z-40 sticky top-[77px] h-[36px] bg-[rgb(9,9,9)] border-b border-[rgb(40,40,40)]/40 container mx-auto flex">
       {props.children}
     </nav>
   );
